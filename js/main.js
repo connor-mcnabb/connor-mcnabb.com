8d23a5d67a14f658e7ffa849fbea8d2ab498991b
Connor McNabb <mcnabbcd@gmail.com>
2013-01-14 14:02:56 -0600

    Website Redesign
    See old website for previous version

---
+++ js/main.js
@@ -0,0 +1,59 @@
+ 
+jQuery(function() {
+	// Index Page
+	$("button.work-items").live('click', function(e){
+		e.preventDefault();
+		var selected = $(this).attr("name");
+		var position = $("section#work").offset().top;
+		var offset = position - $("a#title").height();
+			$("article#"+selected).hide(0, function(e){
+				$("article#"+selected).removeClass("visuallyhidden");
+				$("article#"+selected).fadeIn(300, function(e){
+				$("body").animate({scrollTop: offset}, 600);
+			});
+			$("#worklist").fadeOut(200);
+			$("button#thumbnails").fadeIn(200);	
+		});
+	});
+	$("button#thumbnails").live('click', function(e){
+		e.preventDefault();
+		$("article").fadeOut(200, function(e){
+			$("article").addClass("visuallyhidden");
+			$("article").show(0);
+		});
+		$("button#thumbnails").fadeOut(200);
+		$("#worklist").fadeIn(200);
+	});
+	$("button#toggle-nav").live('click', function(e){
+		$(this).toggleClass('inactive');
+  		$('nav > ul').toggle();
+	});
+	$("nav > ul > li > a").live('click', function(e){
+		if (!$("button#toggle-nav").hasClass('inactive')){
+			$("nav > ul").toggle();
+			$("button#toggle-nav").toggleClass('inactive');
+		}
+	});
+	if ($("html").hasClass("no-touch")){
+		$(window).bind('scroll resize', function(e){
+			var offset = $("#main-wrapper").offset();
+			if ($(window).scrollTop() > offset.top){
+				var hheight = $("header").height();
+				var hh = hheight + 12;
+				$("header").css({
+					'position': 'fixed',
+					'top': '0'
+				});
+				$('#main-wrapper').css({
+					'padding-top': hh + 'px'
+				});
+			}
+			else{
+				$("header").css({'position': 'static'});
+				$('#main-wrapper').css({
+					'padding-top': '0px'
+				});
+			}
+		});	
+	}
+});
---
+++ js/plugins.js
@@ -0,0 +1,82 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+/*global jQuery */
+/*! 
+* FitText.js 1.1
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license 
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+
+(function( $ ){
+  
+  $.fn.fitText = function( kompressor, options ) {
+     
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+  
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this); 
+        
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+        
+      // Call on resize. Opera debounces their resize by default. 
+      $(window).on('resize', resizer);
+        
+    });
+
+  };
+
+})(jQuery);
+
+jQuery(document).ready(function($) {
+ 
+  $(".scroll").click(function(event){
+    if ($(this).is("#title")){
+      $("html,body").animate({scrollTop: 0}, 600);
+    } 
+    else{
+      var trueOffset = $(this.hash).offset().top - $('a#title').height();
+      event.preventDefault();
+      $('html,body').animate({scrollTop:trueOffset}, 600);
+    }
+  });
+});
